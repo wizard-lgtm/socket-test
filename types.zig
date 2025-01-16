19f211ca6c8422d21c10725d140ba0859a945761
wizard-lgtm <rippiten23@gmail.com>
2025-01-16 23:00:52 +0300

    client errors, Message structs added

--- types.zig
+++ types.zig
@@ -1,4 +1,10 @@
 pub const Commands = enum {
     CONNECT, // connect
     DISCONNECT,
+    SEND,
+};
+pub const ClientErrors = error{ BadRequest, UnknownCommand };
+pub const Message = struct {
+    command: Commands,
+    value: []const u8, //
 };
